4a7bbd5324643eff451d5760b40ad54bd405717e
ameya <ameyawarang203@gmail.com>
2024-08-15 18:16:32 +0530

    addition to server.js 1

--- server.js
+++ server.js
@@ -1,4 +1,40 @@
 const express = require ('express');
-const app = express();
+const jwt = require('jsonwebtoken');
+const z = require('zod');
+const bodyParser = require('body-parser');
 
-app.listen(3000);
+
+const app = express();
+app.use(bodyParser.json());
+
+// user-registeration route
+app.get('/register', async(req,res)=>{
+    const{name,username,email,password} = req.body;
+
+    // now check if the user exists in the database
+    try{
+        const emailExists = await User.findone({email});
+        if(emailExists) return res.status(400).send("email already exists");
+
+        const user = new User({
+            name,
+            username,
+            email,
+            password
+        });
+
+        // create another object to save it.
+        const saveuser = await user.save();
+        res.send({msg :'user created succesfully'});
+    }catch(err){
+        res.status(400).send(err);
+    }
+})
+
+
+
+
+
+app.listen(3000, ()=> {
+    console.log("server created succesfully");
+});
